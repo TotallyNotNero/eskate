984faa5b6c46f83e0fc37326f229578b83856907
TotallyNotNero <neromisis2015@gmail.com>
2023-12-06 13:42:30 -0600

    maybe make it work?

--- eskate.cpp
+++ eskate.cpp
@@ -9,6 +9,8 @@
 #include <thread>
 #include <unistd.h>
 #include <SDL2/SDL.h>
+#include <math/RTime.h>
+#include <math/PIDController.h>
 
 using namespace ctre::phoenix;
 using namespace ctre::phoenix::platform;
@@ -16,40 +18,25 @@ using namespace ctre::phoenix::motorcontrol;
 using namespace ctre::phoenix::motorcontrol::can;
 using namespace std;
 
-
+// CAN Bus
 string interface = "can0";
-TalonFX falcon(0, interface);
-
-void initialize() {
-	// Set the Falcon to inverted
-	// so forward is green light.
-	falcon.SetInverted(true);
-
-	// Set neutral mode to coast initially
-	// should it be brake? hmm
-	falcon.SetNeutralMode(NeutralMode::Coast);
-
-	cout << "Initialization Complete." << endl;
-}
-
-void drive(double power) {
-	falcon.Set(ControlMode::PercentOutput, power);
-}
 
 /** simple wrapper for code cleanup */
-void sleepApp(int ms) {
+void delay(int ms) {
 	std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
-int main() {	
-	// Comment out the call if you would rather use the automatically running diag-server, note this requires uninstalling diagnostics from Tuner. 
-	// c_SetPhoenixDiagnosticsStartTime(-1); // disable diag server, instead we will use the diag server stand alone application that Tuner installs
+int main() {
 
-	// setup Falcon
-	initialize();
+	delay(800); // Delay to initialize SocketCAN first
+
+	TalonFX falcon(0, interface);
+
+	falcon.SetInverted(true);
+	falcon.SetNeutralMode(NeutralMode::Coast);
 
 	while (true) {
-		drive(0.2);
+		falcon.Set(ControlMode::PercentOutput, 0.2);
 	}
 
 	return 0;
