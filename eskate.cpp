f5f854e91798e29eee274631f2aba90074c15f0e
TotallyNotNero <Neromisis2015@gmail.com>
2023-12-17 01:06:42 -0600

    Update eskate.cpp

--- eskate.cpp
+++ eskate.cpp
@@ -48,6 +48,9 @@ int main() {
 	falcon.SetInverted(false);
 	falcon.SetNeutralMode(NeutralMode::Brake);
 
+	// Configure motor-based PID values.
+	// Ensures that if we don't hit the desired output,
+	// the motor will try to make up for that error.
 	falcon.Config_kP(0, 0.25);
 	falcon.Config_kI(0, 0.0025);
 	falcon.Config_kD(0, 0.1);
